6de29c2c219c7e3a1eb87a54af9d8d984b5f56db
Ferry Jérémie <ferryjeremie@free.fr>
2023-02-20 13:22:03 +0100

    update to clap 4.1

--- machmap.fish
+++ machmap.fish
@@ -1,4 +1,4 @@
 complete -c machmap -s e -d 'output to a specific extension name (like png)' -r
 complete -c machmap -s s -d 'return list of supporting conversion' -r
-complete -c machmap -s h -l help -d 'Print help information'
-complete -c machmap -s V -l version -d 'Print version information'
+complete -c machmap -s h -l help -d 'Print help'
+complete -c machmap -s V -l version -d 'Print version'
